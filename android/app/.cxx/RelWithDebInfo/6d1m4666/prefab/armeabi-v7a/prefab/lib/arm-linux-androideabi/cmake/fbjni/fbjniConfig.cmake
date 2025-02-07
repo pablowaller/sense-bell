b1b2ac16ce11d246a18ff4cddd8fd07bfde63ad0
PabloWaller <[email protected]>
2025-02-07 15:57:11 -0300

    ignore android and new commit

--- android/app/.cxx/RelWithDebInfo/6d1m4666/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Pablo/.gradle/caches/8.10.2/transforms/47d6ba6d3a183066b3b89a675cc4abe3/transformed/fbjni-0.6.0/prefab/modules/fbjni/libs/android.armeabi-v7a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Pablo/.gradle/caches/8.10.2/transforms/47d6ba6d3a183066b3b89a675cc4abe3/transformed/fbjni-0.6.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
